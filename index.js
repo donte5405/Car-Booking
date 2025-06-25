4b539a668bf608ce7a12ef784519249f18b6df28
donte5405 <donte5405@outlook.com>
2025-06-25 21:50:24 +0700

    Update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {
   Node,
   Percent,
   Px,
+  TextArea,
 } from "./dandelion/dandelion.js";
 import { UseDefaultTheme } from "./dandelion/default.css.js";
 
@@ -38,7 +39,7 @@ Dandelion((body) => {
                     ._HorLeft()
                     ._Text("ชื่อผู้ที่ร้องขอ"),
                   new InputText("Text")
-                    ._PlaceholderText("กรอกชื่อ - นามสกุลของท่าน")
+                    ._PlaceholderText("กรอกชื่อ - นามสกุลของท่าน"),
                 ),
               new Node("RequesterEmail")
                 ._Add(
@@ -46,7 +47,9 @@ Dandelion((body) => {
                     ._HorLeft()
                     ._Text("อีเมลของผู้ที่ร้องขอ"),
                   new InputEmail("Text")
-                    ._PlaceholderText("กรอก E-mail ของท่าน เช่น johndoe@gmail.com"), 
+                    ._PlaceholderText(
+                      "กรอก E-mail ของท่าน เช่น johndoe@gmail.com",
+                    ),
                 ),
               new Node("RequesterDepartment")
                 ._Add(
@@ -54,10 +57,111 @@ Dandelion((body) => {
                     ._HorLeft()
                     ._Text("แผนกของผู้ที่ร้องขอ"),
                   new DropDownMenu("Text")
-                    ._Class("dandelion-inputtext")
                     ._Options(
-                      new DropDownOption("erd", "ERD - แผนกห้องฉุกเฉิน"),
+                      new DropDownOption("แผนกห้องฉุกเฉิน", "ERD - แผนกห้องฉุกเฉิน"),
+                      new DropDownOption("แผนกรังสีวินิจฉัย", "RAD - แผนกรังสีวินิจฉัย"),
+                      new DropDownOption(
+                        "แผนกตรวจสุขภาพ",
+                        "HCC - แผนกตรวจสุขภาพ",
+                      ),
+                      new DropDownOption("แผนกการเงิน", "OIC - แผนกการเงิน"),
+                      new DropDownOption("แผนกเภสัชกรรม", "PHD - แผนกเภสัชกรรม"),
+                      new DropDownOption("แผนกผู้ป่วยนอก", "OPD - แผนกผู้ป่วยนอก"),
+                      new DropDownOption(
+                        "แผนกเด็ก (กุมารเวช)",
+                        "PED - แผนกเด็ก (กุมารเวช)",
+                      ),
+                      new DropDownOption(
+                        "แผนกบริการส่วนหน้า (Regis)",
+                        "CRM - แผนกบริการส่วนหน้า (Regis)",
+                      ),
+                      new DropDownOption(
+                        "แผนกประกันและสิทธิประโยชน์",
+                        "URD - แผนกประกันและสิทธิประโยชน์",
+                      ),
+                      new DropDownOption(
+                        "แผนกเคลื่อนย้ายผู้ป่วย (Porter)",
+                        "PTD - แผนกเคลื่อนย้ายผู้ป่วย (Porter)",
+                      ),
+                      new DropDownOption("แผนกคลังยา", "INV - แผนกคลังยา"),
+                      new DropDownOption(
+                        "แผนกคลังเวชภัณฑ์ปลอดเชื้อ",
+                        "CSD - แผนกคลังเวชภัณฑ์ปลอดเชื้อ",
+                      ),
+                      new DropDownOption(
+                        "แผนกหอผู้ป่วยวิกฤต",
+                        "ICU - แผนกหอผู้ป่วยวิกฤต",
+                      ),
+                      new DropDownOption(
+                        "แผนกห้องคลอดและเด็กแรกเกิด",
+                        "LRN - แผนกห้องคลอดและเด็กแรกเกิด",
+                      ),
+                      new DropDownOption("แผนกห้องผ่าตัด", "ORD - แผนกห้องผ่าตัด"),
+                      new DropDownOption(
+                        "แผนกศูนย์ปฏิบัติการสวนหลอดเลือดหัวใจ",
+                        "CLD - แผนกศูนย์ปฏิบัติการสวนหลอดเลือดหัวใจ",
+                      ),
+                      new DropDownOption("แผนกโภชนาการ", "DND - แผนกโภชนาการ"),
+                      new DropDownOption(
+                        "แผนกห้องปฏิบัติการชันสูตร",
+                        "LBD - แผนกห้องปฏิบัติการชันสูตร",
+                      ),
+                      new DropDownOption(
+                        "แผนกเวชศาสตร์ฟื้นฟูและกายภาพบำบัด",
+                        "RTD - แผนกเวชศาสตร์ฟื้นฟูและกายภาพบำบัด",
+                      ),
+                      new DropDownOption(
+                        "แผนกเทคโนโลยีสารสนเทศ",
+                        "ITD - แผนกเทคโนโลยีสารสนเทศ",
+                      ),
+                      new DropDownOption("แผนกบัญชี", "ACC - แผนกบัญชี"),
+                      new DropDownOption(
+                        "แผนกสื่อสารการตลาด",
+                        "MKD - แผนกสื่อสารการตลาด",
+                      ),
+                      new DropDownOption("สำนักผู้ตรวจการ", "SUP - สำนักผู้ตรวจการ"),
+                      new DropDownOption(
+                        "แผนกทรัพยากรบุคคล",
+                        "HRD - แผนกทรัพยากรบุคคล",
+                      ),
+                      new DropDownOption("สำนักเลขานุการ", "SEC - สำนักเลขานุการ"),
+                      new DropDownOption("สำนักผู้บริหาร", "EXC - สำนักผู้บริหาร"),
+                      new DropDownOption("องค์กรแพทย์", "MAO - องค์กรแพทย์"),
+                      new DropDownOption("แผนกจัดซื้อ", "PUR - แผนกจัดซื้อ"),
+                      new DropDownOption(
+                        "แผนกศูนย์คุณภาพและควบคุมเอกสาร",
+                        "QMD - แผนกศูนย์คุณภาพและควบคุมเอกสาร",
+                      ),
+                      new DropDownOption(
+                        "ฝ่ายสนับสนุนทั่วไป",
+                        "GSD - ฝ่ายสนับสนุนทั่วไป",
+                      ),
+                      new DropDownOption(
+                        "แผนกเครื่องมือแพทย์",
+                        "MED - แผนกเครื่องมือแพทย์",
+                      ),
+                      new DropDownOption(
+                        "แผนกหอผู้ป่วยใน ชั้น 5",
+                        "WA5 - แผนกหอผู้ป่วยใน ชั้น 5",
+                      ),
+                      new DropDownOption(
+                        "แผนกหอผู้ป่วยใน ชั้น 6",
+                        "WA6 - แผนกหอผู้ป่วยใน ชั้น 6",
+                      ),
+                      new DropDownOption(
+                        "ผู้อำนวยการโรงพยาบาล",
+                        "ผู้อำนวยการโรงพยาบาล",
+                      ),
                     ),
+                ),
+              new Node("RequesterReasons")
+                ._Add(
+                  new Label("Lb", "p")
+                    ._HorLeft()
+                    ._Html(HTMLElement, (html) => html.innerHTML = "<b>เหตุผลที่ขอใช้รถ</b>"),
+                  new TextArea("Text")
+                    ._LockWidth(Percent(100))
+                    ._MinHeight(Px(200)),
                 ),
             ),
         ),
